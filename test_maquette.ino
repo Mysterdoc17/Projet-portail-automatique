81c5a79e1f18c891bb0e2eb7c995bc9c34196c3d
Jeremie B <51958809+ratoujemie8617@users.noreply.github.com>
2021-05-10 14:53:06 +0200

    programme fonctionneùe

---
+++ test_maquette.ino
@@ -0,0 +1,38 @@
+
+int moteur1 = 2;
+int moteur2 = 3;
+int capteur1_FDC = 4;
+int capteur2_FDC = 5;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(capteur1_FDC, INPUT);
+  pinMode(moteur1, OUTPUT);
+  pinMode(moteur2, OUTPUT);
+}
+void ouverture()
+{
+  // ouvrir le portail juqu'au capteur de fin de course
+  while ((digitalRead(capteur1_FDC)) != HIGH)
+  {
+    digitalWrite(moteur1, HIGH);
+    digitalWrite(moteur2, LOW);
+  }
+  digitalWrite(moteur1, LOW);
+  digitalWrite(moteur2, LOW);
+}
+
+void fermeture ()
+{
+  while ((digitalWrite(capteur2_FDC)) != HIGH)
+  {
+    digitalWrite(moteur1, LOW);
+    digitalWrite(moteur2, HIGH);
+  }
+  digitalWrite(moteur1, LOW);
+  digitalWrite(moteur2, LOW);
+}
+
+void loop() {
+  
+}
