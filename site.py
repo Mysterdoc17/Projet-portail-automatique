608ebbfbab6cefb8d25cf2f3262be3775ec05b4f
Jeremie B <51958809+ratoujemie8617@users.noreply.github.com>
2021-05-17 20:37:13 +0200

    Update and rename import pymysql.py to site.py

---
+++ site.py
@@ -0,0 +1,66 @@
+from datetime import datetime
+from flask import render_template, Flask
+from Projet_portail_automatique import app
+import requests
+import pymysql
+
+app = Flask(__name__)
+
+
+connexion = pymysql.connect(host="mysql-joske-jones.alwaysdata.net", port=3306, user="valtaudh@gmail.com", passwd="leahugo1805", db="joske-jones_portail-badges", autocommit=True)
+
+@app.route('/')
+@app.route('/home')
+def home():
+    return render_template(
+        'page_accueil_site.html',
+        title='page principale du site',
+        year=datetime.now().year,
+        message='bienvenue sur la page d\'accueil !'
+    )
+
+@app.route('/page affichage utilisateurs')
+def affichage():
+    affichage = 'SELECT * FROM "badges"'
+    return render_template(
+        'page_affichage_utilisateurs.html',
+        title='Page affichage utilisateurs',
+        year=datetime.now().year,
+        message='page d\'affichage des utilisateurs'
+    )
+
+@app.route('/page modification utilisateurs')
+def modification():
+    if request.method == 'POST':
+        nom_python = request.values["name"]
+        adresse_python = request.values["address"]
+        telephone_python = request.values["phone"]
+        badge_python = request.values["card"]
+
+       modification = "UPDATE badges SET "
+    return render_template(
+        'page_modification_utilisateurs.html',
+        title='page modification utilisateurs',
+        year=datetime.now().year,
+        message='page de modification des utilisateurs'
+    )
+
+@app.route('/page création utilisateurs')
+def creation():
+    if request.method =='POST':
+        nom_utilisateur = request.values ['nouveau_nom_html']
+        adresse_utilisateur = request.values["nouvelle_adresse_html"]
+        telephone_utilisateur = request.values["nouveau_telephone_html"]
+        badge_utilisateur = request.values["nouveau_badge_html"]
+
+        creation = "INSERT INTO badges VALUES(NULL," + nom_utilisateur + "," + adresse_utilisateur + "," + telephone_utilisateur + "," + badge_utilisateur + ";"
+
+    return render_template(
+        'page_creation_utilisateurs.html',
+        title='page création utilisateurs',
+        year=datetime.now().year,
+        message='page de création des utilisateurs'
+    )
+        
+if __name__ == '__main__':
+    app.run(debug=True)
